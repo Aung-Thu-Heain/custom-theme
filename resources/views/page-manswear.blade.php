812004939492a40a18679560a7bebdf0710b1c1d
Aung-Thu-Heain <mrfoethu@gmail.com>
2025-04-08 01:46:14 +0630

    manswear

--- resources/views/page-manswear.blade.php
+++ resources/views/page-manswear.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+
 <section class="hero">
   <div id="manswear-hero-section" class="w-auto relative  bg-cover bg-no-repeat " style="height: 601px;">
       <div class="absolute inset-0 bg-black/70"></div>
@@ -33,9 +34,115 @@
   </div>
 </section>
 
-<section class="signature-style bg-primary text-white">
-     <div class="max-w-7xl text-center mx-auto">
-      <h1 class="py-20 text-[36px]">Signature Styles</h1>
+<section class="signature-style bg-secondary  text-white">
+     <div class="max-w-7xl py-30 text-center mx-auto">
+        <h1 class="pb-20 text-[36px]">Signature Styles</h1>
+
+        <div class=" text-center  border-b border-gray-20 text-gray-400 border-gray-700">
+          <ul class="flex flex-wrap justify-center -mb-px text-[24px] leading-[39px]">
+              <li class="mx-12">
+                  <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Suit</a>
+              </li>
+              <li class="mx-12">
+                  <a href="#" class="inline-block p-4 text-white border-b-2 border-white rounded-t-lg active " aria-current="page">Shirt</a>
+              </li>
+              <li class="mx-12">
+                  <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 ">Jacket</a>
+              </li>
+              <li class="mx-12">
+                  <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 ">Pant</a>
+              </li>
+              <li class="mx-12">
+                <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 ">Vest</a>
+            </li>
+            <li class="mx-12">
+              <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 ">OverCoat</a>
+          </li>
+          </ul>
+        </div>
+        <div class="max-w-6xl grid  mt-5  md:grid-cols-4 mx-auto gap-4">
+            <div class="">
+              <img src="{{Vite::asset('resources/images/manswear/Rectangle-107.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-108.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                  <img src="{{Vite::asset('resources/images/manswear/Rectangle-109.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-110.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-111.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-112.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-113.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-114.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-115.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-116.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/manswear/Rectangle-117.png')}}" class="object-cover w-auto h-auto" alt="">
+            </div>
+            <div class="">
+              <img src="{{Vite::asset('resources/images/manswear/Rectangle-118.png')}}" class="object-cover w-auto h-auto" alt="">
+          </div>
+        </div>
      </div>
 </section>
+
+<section class="bg-primary fabric-selection">
+    <div class="max-w-7xl text-white mx-auto px-10 py-20">
+      <h1 class="text-[35px]">Fabric Selection</h1>
+      <span class="text-[24px]">Handpicked Luxury, Woven to Perfection.</span>
+      <div class="grid grid-cols-2 md:grid-cols-7 gap-1 mt-20">
+        <div class="relative">
+            <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-1.png')}}" alt="">
+            <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#aqua5</div>
+        </div>
+        <div class="relative">
+            <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-2.png')}}" alt="">
+            <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#Elegante3</div>
+        </div>
+        <div class="relative">
+          <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-3.png')}}" alt="">
+          <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#Elegante41</div>
+        </div>
+        <div class="relative">
+            <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-4.png')}}" alt="">
+            <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#Lining2</div>
+        </div>
+        <div class="relative">
+            <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-5.png')}}" alt="">
+            <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#Lumino2</div>
+        </div>
+        <div class="relative">
+            <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-6.png')}}" alt="">
+            <div class="absolute bottom-0 left-0 right-0 text-white p-2 ">#Lining34</div>
+        </div>
+        <div class="relative">
+          <div class="absolute inset-0 bg-black/90"></div>
+          <img class="h-[145px] w-full object-cover rounded-lg" src="{{Vite::asset('resources/images/manswear/fabric-7.png')}}" alt="">
+          <div class="absolute inset-0 flex  items-center justify-between text-white text-center px-6">
+            <p class=" mb-2 font-normal  " data-aos="fade-left">50+<br>Handpicked<br>Fabrics</p>
+            <img src="{{Vite::asset('resources/images/icons/ri-arrow-right-line.png')}}" alt="">
+        </div>
+        </div>
+      </div>
+
+    </div>
+<section>
+
+  @include('partials.appointment')
+
 @endsection
---
+++ resources/views/partials/appointment.blade.php
@@ -0,0 +1,12 @@
+<section class="apppointment">
+  <div id="appointment" class="w-auto relative  bg-cover bg-bottom bg-no-repeat" style="height: 500px;">
+      <div class="absolute inset-0 bg-black/70"></div>
+      <div class="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-6">
+          <p class="text-[36px] font-bold mb-2" data-aos="zoom-in">Your Perfect Suit Starts Here!</p>
+          <h1 class="font-normal text-[18px] mb-2" data-aos="zoom-out"> Let us craft your signature style. Schedule your fitting today.</h1>
+          <a href="#" class="bg-white text-black mt-6 px-6 py-3 rounded-lg font-normal text-[16px] hover:bg-gray-200 transition" data-aos="flip-left">
+            Book An Appointment
+          </a>
+      </div>
+  </div>
+</section>
